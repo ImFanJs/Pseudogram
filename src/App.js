035b3ca18c4dfa6a5822b3981dee216c1e95bb70
Jesco Marketing <jesco.marketing@gmail.com>
2017-02-14 16:46:00 -0600

    Add firebase and login component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      user: null
+    }
+  }
+
+  componentWillMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user })
+    })
+  }
+
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(result => console.log(`Error: {error.code}: {error.message}`))
+  }
+  handleLogout () {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(result => console.log(`Error: {error.code}: {error.message}`))
+  }
+
+  renderLoginButton () {
+    if (this.state.user) {
+      return (
+        <div>
+          <img width='100' src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p className='App-intro'>
+             Hola {this.state.user.displayName}
+          </p>
+          <button onClick={this.handleLogout.bind(this)}>Cerrar sesion</button>
+        </div>
+      )
+    } else {
+      return (
+        <button onClick={this.handleAuth.bind(this)}>Login con Google</button>
+      )
+    }
+  }
+
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
           <h2>Pseudogram</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className='App-intro'>
+          {this.renderLoginButton()}
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import firebase from 'firebase'
+import App from './App'
+import './index.css'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCPtxFZfH2aQ8RUdKMYmbzL91-d-0pORTA',
+  authDomain: 'pseudogram-13b51.firebaseapp.com',
+  databaseURL: 'https://pseudogram-13b51.firebaseio.com',
+  storageBucket: 'pseudogram-13b51.appspot.com',
+  messagingSenderId: '447178987908'
+})
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
